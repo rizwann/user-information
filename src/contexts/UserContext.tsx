428072cc9b877e946f6774f22507fb9da132a2d9
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-09-22 21:19:51 +0200

    ADD: setup UserContext

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { User } from "../types";
+
+type UserContextType = {
+  users: User[];
+};
+
+type UserProviderProps = {
+  children: React.ReactNode;
+};
+
+// context for user data
+const UserContext = createContext<UserContextType>({
+  users: [],
+});
+
+// a custom hook useUser for consuming our UserContext
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    // mock-fetch users from api
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("/api/users");
+        const users = await res.json();
+        setUsers(users);
+      } catch (error) {
+        console.log("Error fetching users data", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const contextValue: UserContextType = {
+    users,
+  };
+
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+};

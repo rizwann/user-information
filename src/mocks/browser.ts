7101a5714e4c02cbfa92e866b042bdf9909cbd9e
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-09-23 20:44:17 +0200

    UPDATE: change font to PT Sans

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,7 @@
 import { rest, setupWorker } from "msw";
 import { users } from "./data/usersData";
 export const worker = setupWorker(
-  rest.get("/api/users", (req, res, ctx) => {
+  rest.get("/api/users", (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(users));
   })
 );
